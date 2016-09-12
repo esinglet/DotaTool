d4533e631d07e861255fa28261e43cd5a06603fc
Evan Singleton <esinglet@ualberta.ca>
2016-09-11 19:08:46 -0600

    API conect,match info

---
+++ src/valve_API_tests.py
@@ -0,0 +1,17 @@
+import dota2api as d2
+from time import sleep
+import json
+
+
+
+def main():
+#Needs enviroment variable set see wiki
+	api = d2.Initialise()
+	#print(api.get_match_details(1000193456))
+	print(json.dumps(api.get_match_details(1000193456), sort_keys=True,indent=15))
+	input()
+
+main()
+
+
+
