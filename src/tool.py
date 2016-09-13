f2094df81f2c6ea8efd25e9da0fed29bc735114e
Evan Singleton <esinglet@ualberta.ca>
2016-09-13 03:34:23 -0600

    Portrate fetching

--- src/tool.py
+++ src/tool.py
@@ -1,31 +1,70 @@
 import dota2api as d2
-from json import dump
+import urllib.request
+import json
+import os
+import re
+
+from time import sleep
+
 
 
 def main():
 #Needs enviroment variable set see wiki
 	api = d2.Initialise()
-
+	urls = []
 	value = input()
 
+	#Make a heroes json file
 	if (value == 'hj'):
 		try:
-			
 			file = open('heroes.json', 'w+')
-			dump(api.get_heroes(), file)
+			json.dump(api.get_heroes(), file)
 			file.close()
-		except  e:
+		except:
 			file.close()
 			raise e
 			sleep(10)
+
+	#Make a heroes formatted txt file
 	elif (value == 'ht'):
 		try:
-			
 			file = open('heroes.txt', 'w+')
-			dump(api.get_heroes(), file, sort_keys=True,indent=15)
+			json.dump(api.get_heroes(), file, sort_keys=True,indent=15)
 			file.close()
-		except  e:
+		except:
 			file.close()
+			raise e
+			sleep(10)
+
+	#Collect hero images 
+	#Requires making a heroes json file first
+	elif (value == "p"):
+		try:
+			fp = open('heroes.json', 'r+')
+			jhero = json.load(fp)
+			fp.close()
+
+			for item in jhero["heroes"]:
+				urls.append([ item["url_full_portrait"], item["url_large_portrait"], item["url_small_portrait"], item["name"]])
+
+			if re.search(r"[a-zA-Z0-9]*\\src", os.getcwd()):
+				os.chdir("..")
+				if (not os.path.exists("res")):
+					os.makedirs("res")
+				os.chdir("res\\")
+
+			if (not os.path.exists("portrates")):
+
+				os.makedirs("portrates")
+				os.makedirs("portrates\\large")
+				os.makedirs("portrates\\small")
+
+			for i in urls:
+				urllib.request.urlretrieve(i[1], "portrates\\large\\"+i[3]+"-large.png")
+				urllib.request.urlretrieve(i[2], "portrates\\small\\"+i[3]+"-small.png")
+
+		except Exception as e:
+			fp.close()
 			raise e
 			sleep(10)
 
